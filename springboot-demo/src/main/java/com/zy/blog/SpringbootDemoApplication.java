2faecdbaca4f29f7cf1c48890ae2d1e553cfbfce
weizhenyan <2550360829@qq.com>
2022-08-08 15:09:08 +0800

    加个注释

--- springboot-demo/src/main/java/com/zy/blog/SpringbootDemoApplication.java
+++ springboot-demo/src/main/java/com/zy/blog/SpringbootDemoApplication.java
@@ -3,7 +3,12 @@ package com.zy.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+/**
+ * SpringbootDemoApplication
+ *
+ * @author 飞燕
+ * @date   2022/8/8 13:02
+ */
 @SpringBootApplication
 @EnableTransactionManagement
 public class SpringbootDemoApplication {
